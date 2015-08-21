4e38c04df04c89a7987e1f4174b8686e17a81f6c
Andrew Coates <acoat@microsoft.com>
2015-08-21 21:46:12 +1000

    Removed alert from controller

--- WeatherOfficeAddinsWeb/App/controller.js
+++ WeatherOfficeAddinsWeb/App/controller.js
@@ -8,7 +8,6 @@ weatherApp.controller('WeatherCtrl', ['$scope', '$http', function ($scope, $http
     $scope.dataExists = false;
 
     $scope.doSearch = function () {
-        alert('Searching');
         $http.get('http://api.openweathermap.org/data/2.5/weather?q=' + $scope.city).success(function (data) {
             $scope.weather = data;
             $scope.dataExists = true;
