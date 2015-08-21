91b4b2caec29a63e4d8da1e6e0ae91cf0ae022c8
Andrew Coates <acoat@microsoft.com>
2015-08-21 21:21:43 +1000

    Initial checkin - working standalone, not working in addin

---
+++ WeatherOfficeAddinsWeb/App/controller.js
@@ -0,0 +1,23 @@
+﻿var weatherApp = angular.module('weatherApp', ['weatherFilters']);
+
+
+weatherApp.controller('WeatherCtrl', ['$scope', '$http', function ($scope, $http) {
+
+    $scope.title = 'Weather';
+    $scope.city = '';
+    $scope.dataExists = false;
+
+    $scope.doSearch = function () {
+        alert('Searching');
+        $http.get('http://api.openweathermap.org/data/2.5/weather?q=' + $scope.city).success(function (data) {
+            $scope.weather = data;
+            $scope.dataExists = true;
+        });
+
+        $scope.insertWeather = function () {
+            console.log('inserting weather\n');
+        };
+
+    };
+
+}]);
